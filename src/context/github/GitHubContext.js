751a7fc83b00fc7660e5fba81e51d068e03ec027
Wail-Sr <ia_srairi@esi.dz>
2023-04-04 17:53:43 +0000

    add the reducer

--- src/context/github/GitHubContext.js
+++ src/context/github/GitHubContext.js
@@ -1,26 +1,40 @@
-import { createContext, useState } from "react";
-import { useContext } from "react";
+import { createContext, useReducer } from "react";
+import GithubReducer from "./GithubReducer";
 
 const GitHubContext = createContext()
 
 const GitHubURL = process.env.REACT_APP_GITHUB_URL
 
 export const GitHubProvider = ({children}) => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
 
+    const initialState = {
+        users: [],
+        loading: false,
+    }
+
+    const [state, dispatch] = useReducer(GithubReducer, initialState)
+
+    // GET OUR USERS? IT'S JUST FOR TEST PURPOSES
     const fetchUsers = async () => {
+
+        dispatch({
+            type: 'SET_LOADING',
+            loadingValue: true
+        })
+
         const response = await fetch(`${GitHubURL}users`)
         const data = await response.json()
 
-        setUsers(data)
-        setLoading(false)
-        console.log(data)
+        dispatch({
+            type: 'GET_USERS',
+            payload: data
+        })
+        
     } 
 
     return <GitHubContext.Provider value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
     }}>
         {children}
--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -2,6 +2,18 @@
 
 const GithubReducer = (state, action) => {
     switch (action.type){
+        case 'GET_USERS':
+            return {
+                ...state,
+                users: action.payload,
+                loading: false,
+            }
+        
+        case 'SET_LOADING':
+            return {
+                ...state,
+                loading: action.loadingValue
+            }
         default:
             return state
     }
